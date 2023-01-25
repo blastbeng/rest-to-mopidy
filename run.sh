9a66f98ab5c6ad1d177ad390ba98e10ce3dc8a1e
blast <rodfval.fv@gmail.com>
2023-01-25 17:08:04 +0100

    test commit

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ mkdir -p ./config
 mkdir -p /run/user/1000/tmp/rest-to-tts
 cp uwsgi.ini ./config/uwsgi.ini
 rm -f /run/user/1000/tmp/rest-to-tts/fakeyou_voices.sqlite
-source .venv/bin/activate; uwsgi --ini /opt/rest-to-mopidy/config/uwsgi.ini --enable-threads
+source .venv/bin/activate; uwsgi --ini ./config/uwsgi.ini --enable-threads
