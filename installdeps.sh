c829de2f56c9d1ddc00ed772cdcdd193ed21c60d
blast <rodfval.fv@gmail.com>
2023-01-26 11:43:55 +0100

    aa

--- installdeps.sh
+++ installdeps.sh
@@ -3,5 +3,4 @@
 source .venv/bin/activate; pip3 install wheel
 source .venv/bin/activate; pip3 install -r requirements.txt
 mkdir -p ./config
-mkdir -p /run/user/1000/tmp/rest-to-tts
 cp uwsgi.ini ./config/uwsgi.ini
