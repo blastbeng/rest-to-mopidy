5769e8f0a540c4b3b4c3cf4fbda84313a6e0665c
blast <rodfval.fv@gmail.com>
2023-01-26 09:56:41 +0100

    fixes

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
-mkdir -p ./config
-mkdir -p /run/user/1000/tmp/rest-to-tts
-cp uwsgi.ini ./config/uwsgi.ini
-rm -f /run/user/1000/tmp/rest-to-tts/fakeyou_voices.sqlite
-source .venv/bin/activate; uwsgi --ini ./config/uwsgi.ini --enable-threads
+/usr/bin/mkdir -p ./config
+/usr/bin/mkdir -p /run/user/1000/tmp/rest-to-tts
+/usr/bin/rm -f /run/user/1000/tmp/rest-to-tts/fakeyou_voices.sqlite || true
+cd /opt/rest-to-mopidy/
+source .venv/bin/activate; uwsgi --ini /uwsgi.ini --enable-threads
+
+
